efd62bc13f6a5280426c326f326788fd4082f813
Drew Bradley <williamandrewb@gmail.com>
2022-01-22 17:50:37 -0700

    Update buttons on user home.

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
 
@@ -18,15 +17,12 @@ const User = () => {
         <h3>You have earned</h3>
         <h1>${userCredit}</h1>
       </div>
-      <Stack 
-        spacing={2} 
-        direction="column"
-        justifyContent="center"
-        alignItems="center">
-        <Button variant="contained">Start New Chore</Button>
-        <Button variant="contained">Completed Chores</Button>
-        <Button variant="contained">Pending Chores</Button>
-      </Stack>
+      <div 
+        className="button-container">
+        <button className="user-button">Start New Chore</button>
+        <button className="user-button">Completed Chores</button>
+        <button className="user-button">Pending Chores</button>
+      </div>
     </div>
   );
 }
